3f179050ea12f5f0f26305942b20ef1c87da5adb
Giulio Procopio <giulioprocopioo@gmail.com>
2023-10-28 21:31:00 +0200

    Add React components structure

---
+++ src/components/Electron.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Electron = () => {
+    return (
+        <svg>
+            <circle cx='100' cy='100' r='40' />
+        </svg>
+    );
+};
+
+export default Electron;
